b0e62c5d876d6b66ef6b345c6a11c2af78ea84ef
Kar-thik-m <Sparrowkarthik007@gmail.com>
2026-06-18 11:09:26 +0530

    Fix Cors Erro

--- backend/index.js
+++ backend/index.js
@@ -6,21 +6,27 @@ import cors from "cors";
 
 const app = express();
 
-app.use(express.json());
-Connection_To_DB();
 dotenv.config();
 
 
-app.use("/api/products", productsRoutes);
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
+
+app.options("*", cors());
+
+app.use(express.json());
+Connection_To_DB();
+
+
+app.use("/api/products", productsRoutes);
+
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-
 app.listen(8000, () => {
-    console.log(`Server started on port `);
+    console.log("Server started on port 8000");
 });
